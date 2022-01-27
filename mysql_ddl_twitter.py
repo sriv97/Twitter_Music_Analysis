b905b7054292b208af7522cab84aa61a52aacb3c
Sri Velagapudi <sri@Sris-Air.lan>
2022-01-27 15:57:58 -0800

    working logic for bulk insert

--- mysql_ddl_twitter.py
+++ mysql_ddl_twitter.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="sri",
-  password="TwitterMusic1!",
+  password="Twittermusic1!",
   database="twitter_music"
 )
 
@@ -11,8 +11,8 @@ mycursor = mydb.cursor()
 
 mycursor.execute("CREATE DATABASE IF NOT EXISTS twitter_music")
 
-mycursor.execute("CREATE TABLE IF NOT EXISTS justin_bieber (author_id INT, \
-    tweet_id INT, \
+mycursor.execute("CREATE TABLE IF NOT EXISTS justin_bieber (author_id BIGINT, \
+    tweet_id BIGINT, \
     text VARCHAR(280) NOT NULL, \
     retweet BOOLEAN, \
     created_at DATETIME \
